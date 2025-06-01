ad8a146153d742c0c3b1867260746a9d62562148
samdanielbaguian <baguiandaniel01@gmail.com>
2025-06-01 00:31:38 -0400

    jwt.js

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,1 +1,35 @@
+const jwt = require("jsonwebtoken");
 
+const ACCESS_TOKEN_EXPIRE = "15m"; // Durée de vie du access token
+const REFRESH_TOKEN_EXPIRE = "7d"; // Durée de vie du refresh token
+
+function generateAccessToken(user) {
+  return jwt.sign(
+    { id: user._id, email: user.email, isPremium: user.isPremium },
+    process.env.JWT_SECRET,
+    { expiresIn: ACCESS_TOKEN_EXPIRE }
+  );
+}
+
+function generateRefreshToken(user) {
+  return jwt.sign(
+    { id: user._id },
+    process.env.JWT_REFRESH_SECRET,
+    { expiresIn: REFRESH_TOKEN_EXPIRE }
+  );
+}
+
+function verifyAccessToken(token) {
+  return jwt.verify(token, process.env.JWT_SECRET);
+}
+
+function verifyRefreshToken(token) {
+  return jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+}
+
+module.exports = {
+  generateAccessToken,
+  generateRefreshToken,
+  verifyAccessToken,
+  verifyRefreshToken,
+};
