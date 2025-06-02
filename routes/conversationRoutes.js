5501fdad60b132a0ff10ba0aa2a1c80a7cb07f88
samdanielbaguian <baguiandaniel01@gmail.com>
2025-06-01 23:09:46 -0400

    conversationRoutes.js

--- routes/conversationRoutes.js
+++ routes/conversationRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getUserConversations, markConversationAsRead } = require('../controllers/conversationController');
 const { sendMessage } = require('../controllers/messageController');
-const auth = require('../middlewares/auth'); // Ton middleware pour req.user
+const auth = require('../middleware/authMiddleware');
 
 router.get('/conversations', auth, getUserConversations);
 router.post('/conversations/read', auth, markConversationAsRead);
